781f886b6578daabe8a5bd6121cf15470b1ffa58
kunish <kunish.butt@gmail.com>
2023-06-10 12:29:22 +0800

    fix: use concrete type definitions instead of any

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,27 @@
-import { Probot } from "probot";
-import { TelegramClient } from "./telegram"
+import { Context, Probot } from "probot";
+import { TelegramClient } from "./telegram";
 
 export default (app: Probot) => {
   // on receive push event
-  app.on("push", async (context) => {
-    console.debug(context)
-    app.log.debug(`received a push event: ${context.payload?.head_commit}`)
-  })
+  app.on("push", async (context: Context<"push">) => {
+    console.debug(context);
+    app.log.debug(`received a push event: ${context.payload?.head_commit}`);
+  });
 
   // on receive issue event
-  app.on("issues.reopened", async (context) => {
+  app.on("issues.reopened", async (context: Context<"issues.reopened">) => {
     const comment = context.issue({
-      "body": "Thanks for opening this issue!",
+      body: "Thanks for opening this issue!",
     });
-  return context.octokit.issues.createComment(comment);
-  })
+    return context.octokit.issues.createComment(comment);
+  });
 
   // on receive star event
-  app.on("star.created", async (context) => {
-    console.debug(context)
-    app.log.debug(`received a new star!`)
+  app.on("star.created", async (context: Context<"star.created">) => {
+    console.debug(context);
+    app.log.debug(`received a new star!`);
 
-    var tg = new TelegramClient(context)
-    tg.sendMsg(
-      `${context.payload.repository.name} received a new star!`
-    )
+    const tg = new TelegramClient(context as unknown as Context);
+    tg.sendMsg(`${context.payload.repository.name} received a new star!`);
   });
 };
--- src/telegram.ts
+++ src/telegram.ts
@@ -1,23 +1,21 @@
-import { Telegram } from 'telegraf';
+import { Context } from "probot";
+import { Telegram } from "telegraf";
 // import { BaseWebhookEvent } from "probot";
 
 export class TelegramClient {
   chat_id: string;
   token: string;
   telegram: Telegram;
-  context: any;
+  context: Context;
 
-  constructor(context: any) {
-    this.token = process.env.TELEGRAM_BOT_TOKEN as string
+  constructor(context: Context) {
+    this.token = process.env.TELEGRAM_BOT_TOKEN as string;
     this.chat_id = process.env.TELEGRAM_CHANNEL_ID as string;
     this.telegram = new Telegram(this.token);
     this.context = context;
-  };
+  }
 
   sendMsg(msg: string) {
-    return this.telegram.sendMessage(
-      this.chat_id,
-      msg
-    )
+    return this.telegram.sendMessage(this.chat_id, msg);
   }
 }
