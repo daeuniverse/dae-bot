342dd97899e7755b1fb3c3410615970c9afa352c
Kevin Yu <31861128+yqlbu@users.noreply.github.com>
2023-06-18 18:08:47 +0800

    feat(pull_request.synchronize): add branch-up-to-date handler (#7)
    * fixture: add pull_request.synchronize.json
    
    * chore(deps): add iso8601-duration
    
    * chore(creds): rotate secrets
    
    * feat: add branch-up-to-date event handler

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Context, Probot } from "probot";
 import { TelegramClient } from "./telegram";
+import * as Duration from "iso8601-duration";
 import kv from "@vercel/kv";
 
 export default (app: Probot) => {
@@ -97,4 +98,117 @@ export default (app: Probot) => {
       ]);
     }
   });
+
+  // on pull_request.synchronize event
+  app.on(
+    "pull_request.synchronize",
+    async (context: Context<"pull_request.synchronize">) => {
+      // TODO:
+      // case_#1: check assignee is present, if not set as pr author
+
+      // TODO:
+      // case_#2: automatically assign label if not present, default label should align with "kind" as part of the pr title
+
+      // case_#3: check if pr_branch is up-to-date, if not, merge remote HEAD branch to the pr branch
+      // https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/proposing-changes-to-your-work-with-pull-requests/keeping-your-pull-request-in-sync-with-the-base-branch
+      var metadata = {
+        repo: context.payload.repository.name,
+        owner: context.payload.organization?.login as string,
+        default_branch: context.payload.repository.default_branch,
+        html_url: context.payload.repository.html_url,
+        pull_request: {
+          ref: context.payload.pull_request.head.ref,
+          author: context.payload.pull_request.user.login,
+          number: context.payload.pull_request.number,
+          updated_at: context.payload.pull_request.updated_at,
+          html_url: context.payload.pull_request.html_url,
+        },
+      };
+
+      app.log.info(
+        `received a pull_request.synchronize event: ${JSON.stringify(metadata)}`
+      );
+
+      // 1.1 get timestamps of the head commit from both the merge_base_branch and pr_branch
+      var commits = await Promise.all([
+        // 1.1.1 check if merge_base_branch and pr_branch are diverged
+        // https://octokit.github.io/rest.js/v18#repos-compare-commits
+        context.octokit.repos.compareCommits({
+          owner: metadata.owner,
+          repo: metadata.repo,
+          head: metadata.pull_request.ref,
+          base: metadata.default_branch,
+        }),
+        // 1.1.2 get timestamp from head commit of the pr_branch
+        // https://octokit.github.io/rest.js/v18#git-get-commit
+        context.octokit.repos.getCommit({
+          repo: metadata.repo,
+          owner: metadata.owner,
+          ref: metadata.pull_request.ref,
+        }),
+      ]);
+
+      var [commits_diff, pr_commit] = commits;
+      var status = commits_diff.data.status;
+      var mbCommitterDate = commits_diff.data.merge_base_commit.commit.committer
+        ?.date as string;
+      var prCommitterDate = pr_commit.data.commit.committer?.date as string;
+      var lastPRCommiter = pr_commit.data.commit.committer?.name as string;
+
+      // 1.2 compare timestamp, if mergeBaseDateAgeTimeout > pr_branch_date && status == diverged; then merge remote HEAD branch to the pr branch
+      const maxDiff = Duration.parse(process.env.PR_MAX_AGE as string);
+
+      var mergeBaseDateAgeTimeout = Duration.end(
+        maxDiff,
+        new Date(mbCommitterDate)
+      );
+      var prBranchDate = new Date(prCommitterDate);
+      var exceedAgeTimeout =
+        mergeBaseDateAgeTimeout > prBranchDate && status == "ahead";
+
+      app.log.info(
+        JSON.stringify({
+          status,
+          exceedAgeTimeout,
+          lastPRCommiter,
+          mergeBaseDateAgeTimeout,
+          prBranchDate,
+        })
+      );
+
+      if (
+        !exceedAgeTimeout &&
+        lastPRCommiter != "GitHub" &&
+        status == "diverged"
+      ) {
+        const msg = `🚗 branch ${metadata.pull_request.ref} is currently out-of-sync in ${metadata.repo}; automatically merge origin/${metadata.default_branch} to keep it up-to-date; url: ${metadata.pull_request.html_url}`;
+
+        // 1.3 write a comment to pr_branch if it is out-of-sync
+        // https://octokit.github.io/rest.js/v18#pulls-create-review-comment
+        await context.octokit.issues.createComment({
+          owner: metadata.owner,
+          repo: metadata.repo,
+          issue_number: metadata.pull_request.number,
+          body: `❌ Your branch is currently out-of-sync to ${metadata.default_branch}. No worry, I will fix it for you.`,
+        });
+
+        // 1.4 merge head branch into pr_branch
+        // https://octokit.github.io/rest.js/v18#repos-merge
+        await context.octokit.repos.merge({
+          owner: metadata.owner,
+          repo: metadata.repo,
+          base: metadata.pull_request.ref,
+          head: metadata.default_branch,
+        });
+
+        // 1.5 audit event
+        app.log.info(msg);
+
+        const tg = new TelegramClient(context as unknown as Context);
+        await tg.sendMsg(msg, [
+          process.env.TELEGRAM_DAEUNIVERSE_AUDIT_CHANNEL_ID as string,
+        ]);
+      }
+    }
+  );
 };
