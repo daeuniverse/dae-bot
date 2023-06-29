a85a27d404c1314695d6cd869c46c572f8137eb3
kev <yqlbu@bu.edu>
2023-06-29 15:01:28 +0800

    mock: mod telegram handler

--- api/mock/telegram.ts
+++ api/mock/telegram.ts
@@ -1,7 +1,7 @@
 import { Telegram } from "telegraf";
 import { awesome_sticker } from "../../src/constant";
 
-export class TelegramClient {
+class TelegramClient {
   telegram: Telegram;
 
   constructor() {
@@ -9,23 +9,23 @@ export class TelegramClient {
   }
 
   async sendMsg(msg: string, channels: string[]) {
-    Promise.all(
-      channels.map((chat) => {
+    return await Promise.all(
+      channels.map((chat) =>
         this.telegram.sendMessage(chat, msg, {
           parse_mode: "Markdown",
           disable_web_page_preview: true,
-        });
-      })
+        })
+      )
     );
   }
 }
 
 export default async (req, res) => {
   const tg = new TelegramClient();
-  await tg.sendMsg("hello", [
+  const response = await tg.sendMsg("hello", [
     process.env.TELEGRAM_DAEUNIVERSE_AUDIT_CHANNEL_ID!,
   ]);
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
-  res.json({ result: "ok!" });
+  res.json({ result: "ok!", response });
 };
--- mock/telegram_test.ts
+++ mock/telegram_test.ts
@@ -1,42 +1,32 @@
 import { Telegram } from "telegraf";
-// import { awesome_sticker } from "../src/constant";
 import dotenv from "dotenv";
 
 class TelegramClient {
-  token: string;
   telegram: Telegram;
 
   constructor() {
-    this.token = process.env.TELEGRAM_BOT_TOKEN as string;
-    this.telegram = new Telegram(this.token);
+    this.telegram = new Telegram(process.env.TELEGRAM_BOT_TOKEN!);
   }
 
   async sendMsg(msg: string, channels: string[]) {
-    var promises = channels.map((chat: string) => {
-      return new Promise((resolve, reject) => {
-        try {
-          resolve(
-            this.telegram.sendMessage(chat, msg, {
-              disable_web_page_preview: true,
-            })
-          );
-        } catch (err) {
-          console.log(err);
-          reject(err);
-        }
-      });
-    });
-
-    await Promise.all(promises);
+    return await Promise.all(
+      channels.map((chat) =>
+        this.telegram.sendMessage(chat, msg, {
+          parse_mode: "Markdown",
+          disable_web_page_preview: true,
+        })
+      )
+    );
   }
 }
 
 const main = async () => {
   dotenv.config({ path: ".env" });
   const tg = new TelegramClient();
-  await tg.sendMsg("nihao!", [
-    process.env.TELEGRAM_DAEUNIVERSE_AUDIT_CHANNEL_ID as string,
+  const response = await tg.sendMsg("nihao!", [
+    process.env.TELEGRAM_INFRA_DEV_CHANNEL_ID as string,
   ]);
+  console.log(response);
 };
 
 main();
