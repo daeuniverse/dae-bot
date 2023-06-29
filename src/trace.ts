50187696c744bc257956ceb95bd2258faa5146ed
kev <yqlbu@bu.edu>
2023-06-29 16:17:18 +0800

    refactor(trace): demise autoInstructment and metricsExporter

--- src/trace.ts
+++ src/trace.ts
@@ -1,10 +1,9 @@
 import opentelemetry from "@opentelemetry/api";
 import * as sdk from "@opentelemetry/sdk-node";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
-import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-proto";
-import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
+// import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-proto";
+// import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
 // import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
-//
 
 const { Resource } = require("@opentelemetry/resources");
 const {
@@ -17,16 +16,16 @@ const otel = new sdk.NodeSDK({
     // optional - collection of custom headers to be sent with each request, empty by default
     headers: {},
   }),
-  metricReader: new PeriodicExportingMetricReader({
-    exporter: new OTLPMetricExporter({
-      url: `${process.env.OTEL_EXPORTER_OTLP_ENDPOINT}/v1/metrics`,
-      // an optional object containing custom headers to be sent with each request
-      headers: {},
-    }),
-    exportIntervalMillis: 30 * 1000, // 30s
-  }),
-  // instrumentations: [getNodeAutoInstrumentations()],
+  // metricReader: new PeriodicExportingMetricReader({
+  //   exporter: new OTLPMetricExporter({
+  //     url: `${process.env.OTEL_EXPORTER_OTLP_ENDPOINT}/v1/metrics`,
+  //     // an optional object containing custom headers to be sent with each request
+  //     headers: {},
+  //   }),
+  //   exportIntervalMillis: 30 * 1000, // 30s
+  // }),
   // Optionally register automatic instrumentation libraries
+  // instrumentations: [getNodeAutoInstrumentations()],
   instrumentations: [],
   resource: new Resource({
     [SemanticResourceAttributes.SERVICE_NAME]: process.env.APP_NAME,
@@ -40,6 +39,8 @@ const otel = new sdk.NodeSDK({
   }),
 });
 
-const tracer = opentelemetry.trace.getTracer(process.env.APP_NAME || "dae-bot");
+const tracer = opentelemetry.trace.getTracer(
+  process.env.APP_NAME || "dae-bot"
+);
 
-export { otel, tracer };
+export {otel, tracer}
