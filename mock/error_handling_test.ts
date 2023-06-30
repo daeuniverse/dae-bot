0ddcb20429ed03734f0977fdde98d614d64e0f42
kev <yqlbu@bu.edu>
2023-06-30 09:46:07 +0800

    fixture(mock): add basic error handling

---
+++ mock/error_handling_test.ts
@@ -0,0 +1,64 @@
+import { Octokit } from "octokit";
+import opentelemetry, { Span, SpanStatusCode } from "@opentelemetry/api";
+import { Resource } from "@opentelemetry/resources";
+import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
+import {
+  BasicTracerProvider,
+  ConsoleSpanExporter,
+  SimpleSpanProcessor,
+} from "@opentelemetry/sdk-trace-base";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
+
+const provider = new BasicTracerProvider({
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: "basic-tracer",
+  }),
+});
+
+// Configure span processor to send spans to the exporter
+const exporter = new OTLPTraceExporter({
+  url: `${process.env.OTEL_EXPORTER_OTLP_ENDPOINT}/v1/traces`,
+  // optional - collection of custom headers to be sent with each request, empty by default
+  headers: {},
+});
+
+provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+
+/**
+ * Initialize the OpenTelemetry APIs to use the BasicTracerProvider bindings.
+ *
+ * This registers the tracer provider with the OpenTelemetry API as the global
+ * tracer provider. This means when you call API methods like
+ * `opentelemetry.trace.getTracer`, they will use this tracer provider. If you
+ * do not register a global tracer provider, instrumentation which calls these
+ * methods will receive no-op implementations.
+ */
+provider.register();
+
+export const handler = async () => {
+  const tracer = opentelemetry.trace.getTracer("basic-tracer");
+  await tracer.startActiveSpan(
+    "app.test.error_handling",
+    async (span: Span) => {
+      try {
+        const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+        const result = await octokit.rest.pulls.listReviews({
+          owner: "daeuniverse",
+          repo: "da",
+          pull_number: 162,
+        });
+        span.addEvent(JSON.stringify(result));
+        span.setStatus({ code: SpanStatusCode.OK });
+      } catch (err: any) {
+        console.log(err);
+        span.recordException(err);
+        span.setStatus({ code: SpanStatusCode.ERROR });
+      }
+
+      span.end();
+    }
+  );
+};
+
+handler();
