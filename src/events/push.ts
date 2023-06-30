12e7d7c1f1fff97843ec52afda8d569c7c4bcf4b
Kevin Yu <31861128+yqlbu@users.noreply.github.com>
2023-06-30 16:51:17 +0800

    refactor(handler/push): add default span and error handling (#50)

--- src/events/push.ts
+++ src/events/push.ts
@@ -1,4 +1,4 @@
-import { Span } from "@opentelemetry/api";
+import { Span, SpanStatusCode } from "@opentelemetry/api";
 import { Probot, Context } from "probot";
 import {
   Handler,
@@ -35,214 +35,242 @@ async function handler(
     }
   );
 
-  try {
-    // case_#1 trigger daed.sync-upstream workflow if new changes are pushed to dae-wing origin/main
-    if (
-      context.payload.ref == "refs/heads/main" &&
-      context.payload.repository.name == "dae-wing"
-    ) {
-      // 1.1 construct metadata from payload
-      const metadata = {
-        repo: "daed",
-        owner: context.payload.organization?.login as string,
-        author: context.payload.sender.login,
-        default_branch: context.payload.repository.default_branch,
-        html_url: context.payload.repository.html_url,
-        head_commit: head_commit,
-      };
+  // case_#1 trigger daed.sync-upstream workflow if new changes are pushed to dae-wing origin/main
+  if (
+    context.payload.ref == "refs/heads/main" &&
+    context.payload.repository.name == "dae-wing"
+  ) {
+    await tracer.startActiveSpan(
+      "app.handler.push.daed_sync_upstream",
+      async (span: Span) => {
+        span.setAttribute(
+          "case",
+          "trigger daed.sync-upstream workflow if new changes are pushed to dae-wing origin/main"
+        );
+        try {
+          // 1.1 construct metadata from payload
+          const metadata = {
+            repo: "daed",
+            owner: context.payload.organization?.login as string,
+            author: context.payload.sender.login,
+            default_branch: context.payload.repository.default_branch,
+            html_url: context.payload.repository.html_url,
+            head_commit: head_commit,
+          };
 
-      await tracer.startActiveSpan(
-        "app.handler.push.daed_sync_upstream.metadata",
-        async (span: Span) => {
-          span.setAttributes({
-            metadata: JSON.stringify(metadata),
-            case: "trigger daed.sync-upstream workflow if new changes are pushed to dae-wing origin/main",
-          });
-          span.end();
-        }
-      );
-
-      // 1.2 trigger daed sync-upstream-source workflow
-      const latestRunUrl = await tracer.startActiveSpan(
-        "app.handler.push.daed_sync_upstream.trigger_workflow",
-        async (span: Span) => {
-          span.setAttribute(
-            "functionality",
-            "trigger daed sync-upstream-source workflow"
+          await tracer.startActiveSpan(
+            "app.handler.push.daed_sync_upstream.metadata",
+            async (span: Span) => {
+              span.setAttribute("metadata", JSON.stringify(metadata));
+              span.end();
+            }
           );
-          // https://octokit.github.io/rest.js/v18#actions-create-workflow-dispatch
-          // https://docs.github.com/en/rest/actions/workflows?apiVersion=2022-11-28#create-a-workflow-dispatch-event
-          const result = await extension.octokit.actions
-            .createWorkflowDispatch({
-              owner: metadata.owner,
-              repo: metadata.repo,
-              workflow_id: "sync-upstream.yml",
-              ref: metadata.default_branch,
-              inputs: {
-                "wing-head": metadata.default_branch,
-                "wing-sync-message": "chore(sync): upgrade dae-wing",
-                "pr-branch": daedSyncBranch,
-              },
-            })
-            .then(() =>
-              // 1.3 get latest workflow run metadata
+
+          // 1.2 trigger daed sync-upstream-source workflow
+          const latestRunUrl = await tracer.startActiveSpan(
+            "app.handler.push.daed_sync_upstream.trigger_workflow",
+            async (span: Span) => {
+              span.setAttribute(
+                "functionality",
+                "trigger daed sync-upstream-source workflow"
+              );
+              // https://octokit.github.io/rest.js/v18#actions-create-workflow-dispatch
+              // https://docs.github.com/en/rest/actions/workflows?apiVersion=2022-11-28#create-a-workflow-dispatch-event
+              const result = await extension.octokit.actions
+                .createWorkflowDispatch({
+                  owner: metadata.owner,
+                  repo: metadata.repo,
+                  workflow_id: "sync-upstream.yml",
+                  ref: metadata.default_branch,
+                  inputs: {
+                    "wing-head": metadata.default_branch,
+                    "wing-sync-message": "chore(sync): upgrade dae-wing",
+                    "pr-branch": daedSyncBranch,
+                  },
+                })
+                .then(() =>
+                  // 1.3 get latest workflow run metadata
+                  // https://octokit.github.io/rest.js/v18#actions-list-workflow-runs
+                  extension.octokit.actions
+                    .listWorkflowRuns({
+                      owner: metadata.owner,
+                      repo: metadata.repo,
+                      workflow_id: "sync-upstream.yml",
+                      per_page: 1,
+                    })
+                    .then((res) => res.data.workflow_runs[0].html_url)
+                );
+              span.end();
+              return result;
+            }
+          );
+
+          // 1.4 audit event
+          await tracer.startActiveSpan(
+            "app.handler.push.daed-sync-upstream.audit_event",
+            async (span: Span) => {
+              span.setAttribute("functionality", "audit event");
+              const msg = `🏗️ a new commit was pushed to dae-wing (${metadata.default_branch}); dispatched ${daedSyncBranch} workflow for daed; url: ${latestRunUrl}`;
+              app.log.info(msg);
+              span.addEvent(msg);
+              await extension.tg.sendMsg(msg, [
+                process.env.TELEGRAM_DAEUNIVERSE_AUDIT_CHANNEL_ID!,
+              ]);
+              span.addEvent(msg);
+              span.end();
+            }
+          );
+        } catch (err: any) {
+          span.recordException(err);
+          span.setStatus({ code: SpanStatusCode.ERROR });
+          return {
+            result: "Ops something goes wrong.",
+            error: err,
+          };
+        }
+
+        span.end();
+        return { result: "ok!" };
+      }
+    );
+  }
+
+  // case_#2 create a pull_request when branch sync-upstream is created and pushed to daed (remote)
+  if (
+    context.payload.before == "0000000000000000000000000000000000000000" &&
+    context.payload.repository.name == "daed" &&
+    context.payload.ref.split("/")[2] == daedSyncBranch
+  ) {
+    await tracer.startActiveSpan(
+      "app.handler.push.daed_sync_upstream",
+      async (span: Span) => {
+        span.setAttributes({
+          case: "create a pull_request when branch sync-upstream is created and pushed to daed (remote)",
+        });
+        try {
+          // 1.1 construct metadata from payload
+          const metadata = {
+            repo: context.payload.repository.name,
+            owner: context.payload.organization?.login as string,
+            author: context.payload.sender.login,
+            default_branch: context.payload.repository.default_branch,
+            head_branch: context.payload.ref.split("/")[2],
+          };
+
+          await tracer.startActiveSpan(
+            "app.handler.push.daed_sync_upstream.metadata",
+            async (span: Span) => {
+              span.setAttributes({
+                metadata: JSON.stringify(metadata),
+                case: "create a pull_request when branch sync-upstream is created and pushed to daed (remote)",
+              });
+              span.end();
+            }
+          );
+
+          // 1.2 fetch latest sync-upstream workflow run
+          const latestWorkflowRun = await tracer.startActiveSpan(
+            "app.handler.push.daed_sync_upstream.fetch_latest_workflow_run",
+            async (span: Span) => {
+              span.setAttribute(
+                "functionality",
+                "fetch latest sync-upstream workflow run"
+              );
               // https://octokit.github.io/rest.js/v18#actions-list-workflow-runs
-              extension.octokit.actions
+              const result = await context.octokit.actions
                 .listWorkflowRuns({
                   owner: metadata.owner,
                   repo: metadata.repo,
                   workflow_id: "sync-upstream.yml",
                   per_page: 1,
                 })
-                .then((res) => res.data.workflow_runs[0].html_url)
-            );
-          span.end();
-          return result;
-        }
-      );
+                .then((res) => res.data.workflow_runs[0].html_url);
+              span.end();
+              return result;
+            }
+          );
 
-      // 1.4 audit event
-      await tracer.startActiveSpan(
-        "app.handler.push.daed-sync-upstream.audit_event",
-        async (span: Span) => {
-          span.setAttribute("functionality", "audit event");
-          const msg = `🏗️ a new commit was pushed to dae-wing (${metadata.default_branch}); dispatched ${daedSyncBranch} workflow for daed; url: ${latestRunUrl}`;
-          app.log.info(msg);
-          span.addEvent(msg);
-          await extension.tg.sendMsg(msg, [
-            process.env.TELEGRAM_DAEUNIVERSE_AUDIT_CHANNEL_ID!,
-          ]);
-          span.addEvent(msg);
-          span.end();
+          // 1.3 create a pull_request with head (sync-upstream) and base (main) for daed
+          const msg = `⏳ daed (origin/${metadata.default_branch}) is currently out-of-sync to dae-wing (origin/${metadata.default_branch}); changes are proposed by @daebot in actions - ${latestWorkflowRun}`;
+          await tracer.startActiveSpan(
+            "app.handler.push.daed_sync_upstream.create_pull_request",
+            async (span: Span) => {
+              span.setAttribute(
+                "functionality",
+                "create a pull_request with head (sync-upstream) and base (main) for daed"
+              );
+              // https://octokit.github.io/rest.js/v18#pulls-create
+              await context.octokit.pulls
+                .create({
+                  owner: metadata.owner,
+                  repo: metadata.repo,
+                  head: metadata.head_branch,
+                  base: metadata.default_branch,
+                  title: "chore(sync): keep upstream source up-to-date",
+                  body: msg,
+                })
+                .then((res) => {
+                  // 1.4 add labels
+                  tracer.startActiveSpan(
+                    "app.handler.push.daed_sync_upstream.create_pull_request.add_labels",
+                    async (span: Span) => {
+                      span.setAttribute("functionality", "add labels");
+                      // https://octokit.github.io/rest.js/v18#issues-add-labels
+                      context.octokit.issues.addLabels({
+                        owner: metadata.owner,
+                        repo: metadata.repo,
+                        issue_number: res.data.number,
+                        labels: ["automated-pr", "chore"],
+                      });
+                      span.end();
+                    }
+                  );
+
+                  // 1.5 add assignee
+                  tracer.startActiveSpan(
+                    "app.handler.push.daed_sync_upstream.create_pull_request.add_assignee",
+                    async (span: Span) => {
+                      span.setAttribute("functionality", "add assignee");
+                      // https://octokit.github.io/rest.js/v18#issues-add-assignees
+                      context.octokit.issues.addAssignees({
+                        owner: metadata.owner,
+                        repo: metadata.repo,
+                        issue_number: res.data.number,
+                        assignees: ["daebot"],
+                      });
+                      span.end();
+                    }
+                  );
+                });
+              span.end();
+            }
+          );
+
+          // 1.6 audit event
+          await tracer.startActiveSpan(
+            "app.handler.push.daed-sync-upstream.audit_event",
+            async (span: Span) => {
+              span.setAttribute("functionality", "audit event");
+              app.log.info(msg);
+              span.addEvent(msg);
+              await extension.tg.sendMsg(msg, [
+                process.env.TELEGRAM_DAEUNIVERSE_AUDIT_CHANNEL_ID!,
+              ]);
+              span.end();
+            }
+          );
+        } catch (err: any) {
+          span.recordException(err);
+          span.setStatus({ code: SpanStatusCode.ERROR });
+          return { result: "Ops something goes wrong.", error: err };
         }
-      );
-    }
-  } catch (err) {
-    return { result: "Ops something goes wrong.", error: JSON.stringify(err) };
+
+        span.end();
+        return { result: "ok!" };
+      }
+    );
   }
 
-  try {
-    // case_#2 create a pull_request when branch sync-upstream is created and pushed to daed (remote)
-    if (
-      context.payload.before == "0000000000000000000000000000000000000000" &&
-      context.payload.repository.name == "daed" &&
-      context.payload.ref.split("/")[2] == daedSyncBranch
-    ) {
-      // 1.1 construct metadata from payload
-      const metadata = {
-        repo: context.payload.repository.name,
-        owner: context.payload.organization?.login as string,
-        author: context.payload.sender.login,
-        default_branch: context.payload.repository.default_branch,
-        head_branch: context.payload.ref.split("/")[2],
-      };
-
-      await tracer.startActiveSpan(
-        "app.handler.push.daed_sync_upstream.metadata",
-        async (span: Span) => {
-          span.setAttributes({
-            metadata: JSON.stringify(metadata),
-            case: "create a pull_request when branch sync-upstream is created and pushed to daed (remote)",
-          });
-          span.end();
-        }
-      );
-
-      // 1.2 fetch latest sync-upstream workflow run
-      const latestWorkflowRun = await tracer.startActiveSpan(
-        "app.handler.push.daed_sync_upstream.fetch_latest_workflow_run",
-        async (span: Span) => {
-          span.setAttribute(
-            "functionality",
-            "fetch latest sync-upstream workflow run"
-          );
-          // https://octokit.github.io/rest.js/v18#actions-list-workflow-runs
-          const result = await context.octokit.actions
-            .listWorkflowRuns({
-              owner: metadata.owner,
-              repo: metadata.repo,
-              workflow_id: "sync-upstream.yml",
-              per_page: 1,
-            })
-            .then((res) => res.data.workflow_runs[0].html_url);
-          span.end();
-          return result;
-        }
-      );
-
-      // 1.3 create a pull_request with head (sync-upstream) and base (main) for daed
-      const msg = `⏳ daed (origin/${metadata.default_branch}) is currently out-of-sync to dae-wing (origin/${metadata.default_branch}); changes are proposed by @daebot in actions - ${latestWorkflowRun}`;
-      await tracer.startActiveSpan(
-        "app.handler.push.daed_sync_upstream.create_pull_request",
-        async (span: Span) => {
-          span.setAttribute(
-            "functionality",
-            "create a pull_request with head (sync-upstream) and base (main) for daed"
-          );
-          // https://octokit.github.io/rest.js/v18#pulls-create
-          await context.octokit.pulls
-            .create({
-              owner: metadata.owner,
-              repo: metadata.repo,
-              head: metadata.head_branch,
-              base: metadata.default_branch,
-              title: "chore(sync): keep upstream source up-to-date",
-              body: msg,
-            })
-            .then((res) => {
-              // 1.4 add labels
-              tracer.startActiveSpan(
-                "app.handler.push.daed_sync_upstream.create_pull_request.add_labels",
-                async (span: Span) => {
-                  span.setAttribute("functionality", "add labels");
-                  // https://octokit.github.io/rest.js/v18#issues-add-labels
-                  context.octokit.issues.addLabels({
-                    owner: metadata.owner,
-                    repo: metadata.repo,
-                    issue_number: res.data.number,
-                    labels: ["automated-pr", "chore"],
-                  });
-                  span.end();
-                }
-              );
-
-              // 1.5 add assignee
-              tracer.startActiveSpan(
-                "app.handler.push.daed_sync_upstream.create_pull_request.add_assignee",
-                async (span: Span) => {
-                  span.setAttribute("functionality", "add assignee");
-                  // https://octokit.github.io/rest.js/v18#issues-add-assignees
-                  context.octokit.issues.addAssignees({
-                    owner: metadata.owner,
-                    repo: metadata.repo,
-                    issue_number: res.data.number,
-                    assignees: ["daebot"],
-                  });
-                  span.end();
-                }
-              );
-            });
-          span.end();
-        }
-      );
-
-      // 1.6 audit event
-      await tracer.startActiveSpan(
-        "app.handler.push.daed-sync-upstream.audit_event",
-        async (span: Span) => {
-          span.setAttribute("functionality", "audit event");
-          app.log.info(msg);
-          span.addEvent(msg);
-          await extension.tg.sendMsg(msg, [
-            process.env.TELEGRAM_DAEUNIVERSE_AUDIT_CHANNEL_ID!,
-          ]);
-          span.end();
-        }
-      );
-    }
-  } catch (err: any) {
-    return { result: "Ops something goes wrong.", error: err };
-  }
-
+  // fallback
   return { result: "ok!" };
 }
